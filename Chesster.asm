c0c188f8d5bd18373c5228985795b98aa690a328
ehime <dodomeki@gmail.com>
2016-05-24 16:51:12 -0700

    Back-rank "rnbqkbnr" 32b patterns

--- Chesster.asm
+++ Chesster.asm
@@ -6,3 +6,13 @@ d equ dword
   cwd
   xchg ax,di
   mov cl,20h
+a:mov eax,52364325h
+  rol eax,cl
+  and al,15
+  stosb
+  mov [di+0eh],si
+  mov [di+5eh],bp
+  or al,8
+  mov [di+6fh],al
+  sub cl,3
+  loop a

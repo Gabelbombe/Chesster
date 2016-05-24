d5e3bd88ba48fe478774a7be58d999511049dcfa
ehime <dodomeki@gmail.com>
2016-05-24 16:52:12 -0700

    Helper comments

--- Chesster.asm
+++ Chesster.asm
@@ -1,11 +1,12 @@
-w equ word
-d equ dword
-  org 100h
-  pusha
-  rep stosb
-  cwd
-  xchg ax,di
-  mov cl,20h
+w equ word                ; 16-bit prettifying helper,Chesslin v1.0 in 2016
+d equ dword               ; 32-bit prettifying helper,fasm assembler syntax
+  org 100h                ; binary ip execution seg start address above psp
+  pusha                   ; para down stack and avoid input buff collisions
+  rep stosb               ; prepare board empty squares assumes ax=0 cx=255
+  cwd                     ; set Black=0=top active player turn, White=8=bot
+  xchg ax,di              ; shorter mov di,ax prepares writing segment base
+  mov cl,20h              ; 32 initialization decoding bit rotations in all
+
 a:mov eax,52364325h
   rol eax,cl
   and al,15
